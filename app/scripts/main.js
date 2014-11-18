5fa491be2063b054a48573de6bb47526258ad455
Phil Helm <phelm@phelm.co.uk>
2014-11-18 23:36:30 +0000

    better

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,3 +21,44 @@ var board = new Array(9);
 var turn = function (player){
 	
 }
+
+var boxes = document.getElementsByClassName('box');
+
+var b0 = document.getElementById('b0'),
+	b1 = document.getElementById('b1'),
+	b2 = document.getElementById('b2'),
+	b3 = document.getElementById('b3'),
+	b4 = document.getElementById('b4'),
+	b5 = document.getElementById('b5'),
+	b6 = document.getElementById('b6'),
+	b7 = document.getElementById('b7'),
+	b8 = document.getElementById('b8');
+
+var boxes
+
+for (var i = 0; i < boxes.length; i++) {
+	boxes[i].onclick = function () {
+		console.log('you clicked b' + i);
+	}
+};
+
+var updateBoard = function () {
+	board.forEach(function(elem, i){
+		boxes[i].innerHTML = elem
+	})
+}
+
+var doMove = function (player, box) {
+	if (board[box] === undefined){
+		console.log(player + ' has gone in box ' + box);
+		board[box] = player;
+		updateBoard();
+	} else {
+		console.log('player ' + board[box] + ' has already gone there, go again');
+
+	}
+}
+
+
+
+var winner = function () {}
