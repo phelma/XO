44d70469580c0cf24639c4719c275885e558c986
Phil Helm <philip.helm@omgplc.com>
2014-11-24 17:28:52 +0000

    yeahyeah

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -24,21 +24,11 @@ var turn = function (player){
 
 var boxes = document.getElementsByClassName('box');
 
-// var b0 = document.getElementById('b0'),
-// 	b1 = document.getElementById('b1'),
-// 	b2 = document.getElementById('b2'),
-// 	b3 = document.getElementById('b3'),
-// 	b4 = document.getElementById('b4'),
-// 	b5 = document.getElementById('b5'),
-// 	b6 = document.getElementById('b6'),
-// 	b7 = document.getElementById('b7'),
-// 	b8 = document.getElementById('b8');
-
-// var boxes
-
 for (var i = 0; i < boxes.length; i++) {
 	boxes[i].onclick = function () {
-		console.log('you clicked ' this.id);
+		console.log('you clicked ' + this.id);
+		doMove('x', this.id);
+		updateBoard();
 	}
 };
 
