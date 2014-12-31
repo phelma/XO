879886a64844e7f1d3ce17738a2ef53fbf220276
Phil Helm <philip.helm@omgplc.com>
2014-12-31 15:18:38 +0000

    some more, it not working yet though, will be soon

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,21 +10,66 @@
 
 */
 
-var lines = [
+var board = {
+	boxes: [],
+	state: function () {
+		return this.boxes.map(function(el){
+			return el.state;
+		});
+	},
+	places: function(piece){
+		var out = [];
+		this.state().forEach(function (elem, index) {
+			if (elem === piece) {out.push(index);}
+		});
+		return out;
+	},
+	lines: [
 	[0,1,2],[3,4,5],[6,7,8], // ---
 	[0,3,6],[1,4,7],[2,5,8], // |||
 	[0,4,8],[2,4,6]          // \/
-];
-
-var board = [];
-var currentPlayer = 'X';
+	],
+	winner: function(){
+		var won = {};
+		players.forEach(function (player) {
+			var pPlaces = this.places(player); //players places
+			this.lines.forEach(function (line) {
+				if (intersectionLength(line, pPlaces) === 3) {
+					won = {
+						player: player,
+						boxes: line
+					};
+				}
+			});
+		}.bind(this));
+		
+	}
+};
+var players = ['X', 'O'];
+var currentPlayer = players[0];
+var showMessage = function(message){
+	console.log(message);
+	messages.innerHTML = message;
+};
+var changePlayer = function(player) {
+	var player = player || currentPlayer;
+	if (player === 'X') {
+		player = 'O';
+	} else if (player === 'O') {
+		player = 'X';
+	}
+	else {throw player + ' is not X or O'};
+	showMessage(player + 's Turn');
+	currentPlayer = player;
+	return player;
+}
 
 function Box(i) {
 	this.i = i;
 	this.id = 'b' + i;
 	this.elem = document.getElementById(this.id);
 	this.elem.onclick = function() {
-		console.log('you clicked ' + this.id);
+		showMessage('you clicked ' + this.id);
 		doMove(currentPlayer, i);
 	};
 	this.place = function(player) {
@@ -35,7 +80,7 @@ function Box(i) {
 }
 
 for (var i = 0; i < 9; i++) {
-	board[i] = new Box(i);
+	board.boxes[i] = new Box(i);
 }
 
 var turn = function(player) {
@@ -43,13 +88,22 @@ var turn = function(player) {
 };
 
 var doMove = function(player, box) {
-	if (board[box].state === undefined) {
-		console.log(player + ' has gone in box ' + box);
-		board[box].place(player);
+	if (board.boxes[box].state === undefined) {
+		showMessage(player + ' has gone in box ' + box);
+		board.boxes[box].place(player);
+		var winner = board.winner();
+		if (winner) {
+			showMessage(winner.player + ' has won');
+		}
+		changePlayer();
 	} else {
-		console.log('player ' + board[box] + ' has already gone there, go again');
+		showMessage('Try again, player ' + boardboxes[box].state + ' has already gone there.');
 	}
 };
+
+var gameOver = function(){
+
+}
 
 var pickBest = function(board) {
 
